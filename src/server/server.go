4d5e7c466294f16f6bc5d546aba8270a278935be
Geniusning <690984649@qq.com>
2019-05-29 23:18:47 +0800

    go init

---
+++ src/server/server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func myHandle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "hello world")
+}
+func main() {
+	http.HandleFunc("/", myHandle)
+
+	http.ListenAndServe(":8080", nil)
+}
