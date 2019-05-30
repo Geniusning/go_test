41dd2284123af7958539a41cdefc74fc7b903494
Geniusning <690984649@qq.com>
2019-05-31 00:01:29 +0800

    add login

--- src/server/server.go
+++ src/server/server.go
@@ -3,13 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 func myHandle(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(w, "hello world")
 }
+func myLoginHandle(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		fmt.Println("username=", r.Form["username"])
+		fmt.Println("password=", r.Form["password"])
+	}
+}
 func main() {
 	http.HandleFunc("/", myHandle)
+	http.HandleFunc("/login", myLoginHandle)
 
 	http.ListenAndServe(":8888", nil)
 }
